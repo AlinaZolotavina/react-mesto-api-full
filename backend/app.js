6ab9ab4fb69a8b3c6908f8008a979bfd99767ec4
Alina Zolotavina <abekmetova@yandex.ru>
2022-08-13 23:35:52 +0400

    LOGGER

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,13 @@ const { login, logout, createUser } = require('./controllers/users');
 const errorHandler = require('./middlewares/errorHandler');
 const { validateUrl } = require('./utils/validateUrl');
 const NotFoundError = require('./errors/not-found-err');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use(cookieParser());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -25,15 +25,20 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use(requestLogger);
 app.use(cors(corsOptions));
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required(),
     password: Joi.string().required().min(6),
   }),
 }), login);
-
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required(),
@@ -43,17 +48,14 @@ app.post('/signup', celebrate({
     avatar: Joi.string().custom(validateUrl, 'custom validation'),
   }),
 }), createUser);
-
 app.delete('/logout', logout);
-
 app.use(router);
-
 app.use('*', (req, res) => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 });
 
+app.use(errorLogger);
 app.use(errors());
-
 app.use(errorHandler);
 
 app.listen(PORT, () => {
